321ba50b594044f64d253eb47b344a23116ce692
Mohamed Ahmed <mohamed.ahmed@live.ca>
2015-01-07 11:16:33 -0500

    editted theme

--- app/editor/editor.js
+++ app/editor/editor.js
@@ -7,6 +7,8 @@ var editor = angular.module('myApp.editor', ['ui.ace']);
 editor.controller('EditorCtrl', ['$scope', function($scope) {
 	$scope.aceLoaded = function(_editor){
 		_editor.setShowPrintMargin(false);
+		_editor.setTheme("ace/theme/monokai");
+	    _editor.getSession().setMode("ace/mode/javascript");
 	};
 }]);
 
