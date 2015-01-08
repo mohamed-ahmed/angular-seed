a20bd0c79fb3aead09720a0005e13cdb32909c99
Mohamed Ahmed <mohamed.ahmed@live.ca>
2015-01-08 15:15:03 -0500

    edited file structure and file explorer UI

--- app/fileExplorer/fileExplorer.js
+++ app/fileExplorer/fileExplorer.js
@@ -14,35 +14,35 @@ fileExplorer.controller('FileExplorerCtrl', ['$scope', function($scope) {
 	$scope.data = [
 	{
 		"id": 1,
-		"title": "1. dragon-breath",
+		"title": "dragon-breath",
 		"nodes": []
 	}, {
 		"id": 2,
-		"title": "2. moiré-vision",
+		"title": "moiré-vision",
 		"nodes": [{
 			"id": 21,
-			"title": "2.1. tofu-animation",
+			"title": "tofu-animation",
 			"nodes": [{
 				"id": 211,
-				"title": "2.1.1. spooky-giraffe",
+				"title": "spooky-giraffe",
 				"nodes": []
 			}, {
 				"id": 212,
-				"title": "2.1.2. bubble-burst",
+				"title": "bubble-burst",
 				"items": []
 			}],
 		}, {
 			"id": 22,
-			"title": "2.2. barehand-atomsplitting",
+			"title": "barehand-atomsplitting",
 			"nodes": []
 		}],
 	}, {
 		"id": 3,
-		"title": "3. unicorn-zapper",
+		"title": "unicorn-zapper",
 		"nodes": []
 	}, {
 		"id": 4,
-		"title": "4. romantic-transclusion",
+		"title": "romantic-transclusion",
 		"nodes": []
 	}
 	];
